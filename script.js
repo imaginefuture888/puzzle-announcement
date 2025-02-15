6258d1638e6fb88eae32451c65756a3122880f51
imaginefuture888 <yssa29@yahoo.com>
2025-02-15 14:56:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (let i = 1; i <= 9; i++) {
         let piece = document.createElement("img");
-        piece.src = `Images/piece${i}.png`;  // Correct path
+        piece.src = `Images/piece${i}.png`;
         piece.classList.add("puzzle-piece");
         piece.dataset.index = i;
 
